b8cc1a10a6d86e7275287357b9b16e8bd17997ef
robbyvan <fzpinus@gmail.com>
2017-01-25 20:45:58 -0800

    homepage style

--- client/controllers/movies.js
+++ client/controllers/movies.js
@@ -2,4 +2,12 @@ var myApp = angular.module('myApp');
 
 myApp.controller('MovieController', ['$scope', '$http', '$routeParams', '$location', function($scope, $http, $routeParams, $location){
   console.log("MovieController Loaded.");
+
+  $scope.getMovies = function(){
+    $http.get('/api/movies').then(function(response){
+      $scope.movies = response.data;
+    });
+  }
+
 }]);
+
